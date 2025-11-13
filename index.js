449b9f0034edea04fe8efcb2b3b29a1560e73b7e
Matthew Hasan <matthewhasan@Matthews-MacBook-Air.local>
2025-11-13 07:56:19 -0600

    set up server and connects form to correct path

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 import express from "express";
+import {dirname} from 'path';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", async (req, res) => {
+    res.sendFile(path.join(__dirname,'Views', 'form1.html'));
+});
+
+app.listen(port, (req, res) => {
+    console.log(`Listening on Port ${port}`);
+});
